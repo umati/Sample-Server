314d2e7ec1e50c0323fc2ad226e4eed054f774b2
Christian von Arnim <christian.von-arnim@isw.uni-stuttgart.de>
2019-12-17 09:29:49 +0100

    Add mutex to prevent parallel access

--- ExampleUmatiServer.cpp
+++ ExampleUmatiServer.cpp
@@ -3,45 +3,45 @@
 #include "src_generated/namespace_iswexample_generated.h"
 #include "src_generated/namespace_umati_generated.h"
 #include "src_generated/iswexample.h"
-#include <open62541/server.h>
 #include <cstdint>
 #include <functional>
-#include <map>
-#include <open62541/types.h>
 #include "NodeValue.hpp"
 #include "NodesMaster.hpp"
 #include "BindValue.hpp"
-
-
-void onReadBuildYear(UA_Server *pServer, const UA_NodeId *sessionId,
-                     void *sessionContext, const UA_NodeId *nodeid,
-                     void *nodeContext, const UA_NumericRange *range,
-                     const UA_DataValue *oldValue) {
-  UA_Variant value;
-  std::uint32_t i = 2019;
-  UA_Variant_setScalar(&value, &i, &UA_TYPES[UA_TYPES_UINT32]);
-  UA_Server_writeValue(pServer, *nodeid, value);
-}
-
-class ISourceVariable {
-
-};
-
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <atomic>
 
 struct MachineIdentification_t{
   std::uint32_t  BuildYear;
   std::string CatalogueName;
 };
 
+
+void simulate(MachineIdentification_t *pInfo, std::atomic_bool &running, std::mutex &accessDataMutex){
+  std::unique_lock<std::remove_reference<decltype(accessDataMutex)>::type> ul(accessDataMutex);
+  ul.unlock();
+  while(running){
+    ul.lock();
+    ++(pInfo->BuildYear);
+    ul.unlock();
+    std::this_thread::yield();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+
+}
+
 int main(int argc, char *argv[]) {
 
   UA_Server *pServer = UA_Server_new();
   UA_ServerConfig_setDefault(UA_Server_getConfig(pServer));
 
-  std::cout << "TestNodesetCompiler" << std::endl;
+  std::cout << "ExampleUmatiServer" << std::endl;
   namespace_umati_generated(pServer);
   namespace_iswexample_generated(pServer);
 
+  std::mutex accessDataMutex;
   NodesMaster n(pServer);
   MachineIdentification_t identificationMachine = {.BuildYear = 2019, .CatalogueName = "ISW Example"};
   bindValue(
@@ -55,8 +55,23 @@ int main(int argc, char *argv[]) {
 
   n.SetCallbacks();
 
-  bool running = true;
-  UA_StatusCode retval = UA_Server_run(pServer, &running);
+  std::atomic_bool running {true};
+  std::thread t(simulate, &identificationMachine, std::ref(running), std::ref(accessDataMutex));
+
+  //UA_Server_run(pServer, &running);
+  UA_Server_run_startup(pServer);
+  std::unique_lock<decltype(accessDataMutex)> ul(accessDataMutex);
+  ul.unlock();
+  while(running)
+  {
+    ul.lock();
+    std::uint16_t timeout = UA_Server_run_iterate(pServer, false);
+    ul.unlock();
+    std::this_thread::yield();
+    std::this_thread::sleep_for(std::chrono::milliseconds(timeout));
+  }
+
+  UA_Server_run_shutdown(pServer);
 
   return 0;
 }

fe0636ef8d6bf722d58849591d051983089126ae
Christian von Arnim <christian.von-arnim@isw.uni-stuttgart.de>
2019-12-17 13:16:53 +0100

    Add Event example

--- ExampleUmatiServer.cpp
+++ ExampleUmatiServer.cpp
@@ -3,6 +3,7 @@
 #include "src_generated/namespace_iswexample_generated.h"
 #include "src_generated/namespace_umati_generated.h"
 #include "src_generated/iswexample.h"
+#include "src_generated/umati_nodeids.h"
 #include <cstdint>
 #include <functional>
 #include "NodeValue.hpp"
@@ -12,19 +13,106 @@
 #include <chrono>
 #include <mutex>
 #include <atomic>
+#include <open62541Cpp/UA_String.hpp>
 
 struct MachineIdentification_t{
   std::uint32_t  BuildYear;
   std::string CatalogueName;
 };
 
+struct NotificationEvent_t {
+  std::string Identifier;
+  std::string Message;
+  std::string SourceName;
+  std::uint16_t Severity;
+};
 
-void simulate(MachineIdentification_t *pInfo, std::atomic_bool &running, std::mutex &accessDataMutex){
+UA_NodeId setupNotificationEvent(UA_Server *pServer, NotificationEvent_t ev){
+  UA_NodeId eventNodeId;
+  UA_StatusCode retval = UA_Server_createEvent(
+      pServer,
+      UA_NODEID_NUMERIC(2, UA_UMATIID_NOTIFICATIONEVENTTYPE),
+      &eventNodeId);
+
+  if(retval != UA_STATUSCODE_GOOD){
+    std::cerr << "Could not create event: " << retval << std::endl;
+    return UA_NODEID_NULL;
+  }
+
+  // Setting the Time is required or else the event will not show up in UAExpert!
+  UA_DateTime eventTime = UA_DateTime_now();
+  UA_Server_writeObjectProperty_scalar(
+      pServer,
+      eventNodeId,
+      UA_QUALIFIEDNAME(0, "Time"),
+      &eventTime,
+      &UA_TYPES[UA_TYPES_DATETIME]
+      );
+  {
+    open62541Cpp::UA_String strIdentifier(ev.Identifier);
+    UA_Variant *val = UA_Variant_new();
+    UA_Variant_clear(val);
+    UA_Variant_setScalarCopy(val, strIdentifier.String, &UA_TYPES[UA_TYPES_STRING]);
+    UA_Server_writeObjectProperty(
+        pServer,
+        eventNodeId,
+      UA_QUALIFIEDNAME(2, "Identifier"),
+      *val
+    );
+  }
+
+  {
+    open62541Cpp::UA_String strIdentifier(ev.SourceName);
+    UA_Variant *val = UA_Variant_new();
+    UA_Variant_clear(val);
+    UA_Variant_setScalarCopy(val, strIdentifier.String, &UA_TYPES[UA_TYPES_STRING]);
+    UA_Server_writeObjectProperty(
+        pServer,
+        eventNodeId,
+        UA_QUALIFIEDNAME(0, "SourceName"),
+        *val
+    );
+  }
+
+  {
+    open62541Cpp::UA_String strMessage(ev.Message);
+    UA_Variant *val = UA_Variant_new();
+    UA_LocalizedText ltxtMessage = UA_LOCALIZEDTEXT("en-en", "");
+    ltxtMessage.text = *strMessage.String;
+    UA_Variant_clear(val);
+    UA_Variant_setScalarCopy(val, &ltxtMessage, &UA_TYPES[UA_TYPES_LOCALIZEDTEXT]);
+    UA_Server_writeObjectProperty(
+        pServer,
+        eventNodeId,
+        UA_QUALIFIEDNAME(0, "Message"),
+        *val
+    );
+  }
+
+  {
+    UA_Variant *val = UA_Variant_new();
+    UA_Variant_clear(val);
+    UA_Variant_setScalarCopy(val, &ev.Severity, &UA_TYPES[UA_TYPES_UINT16]);
+    UA_Server_writeObjectProperty(
+        pServer,
+        eventNodeId,
+        UA_QUALIFIEDNAME(0, "Severity"),
+        *val
+    );
+  }
+
+  return eventNodeId;
+}
+
+void simulate(MachineIdentification_t *pInfo, std::atomic_bool &running, std::mutex &accessDataMutex, UA_Server *pServer){
   std::unique_lock<std::remove_reference<decltype(accessDataMutex)>::type> ul(accessDataMutex);
   ul.unlock();
   while(running){
     ul.lock();
     ++(pInfo->BuildYear);
+    NotificationEvent_t ev {.Identifier = "MyId", .Message="MessageTxt", .SourceName="MySource", .Severity = 500};
+    auto evNodeId = setupNotificationEvent(pServer, ev);
+    auto retval = UA_Server_triggerEvent(pServer, evNodeId, UA_NODEID_NUMERIC(0, UA_NS0ID_SERVER), NULL, UA_TRUE);
     ul.unlock();
     std::this_thread::yield();
     std::this_thread::sleep_for(std::chrono::seconds(1));
@@ -56,11 +144,11 @@ int main(int argc, char *argv[]) {
   n.SetCallbacks();
 
   std::atomic_bool running {true};
-  std::thread t(simulate, &identificationMachine, std::ref(running), std::ref(accessDataMutex));
 
   //UA_Server_run(pServer, &running);
   UA_Server_run_startup(pServer);
   std::unique_lock<decltype(accessDataMutex)> ul(accessDataMutex);
+  std::thread t(simulate, &identificationMachine, std::ref(running), std::ref(accessDataMutex), pServer);
   ul.unlock();
   while(running)
   {

94f91ee235c5bfb4b16173e62d5bf258888cac33
Christian von Arnim <christian.von-arnim@isw.uni-stuttgart.de>
2020-02-10 16:34:30 +0100

    Add reflCpp Library

---
+++ cmake/FindReflCpp.cmake
@@ -0,0 +1,13 @@
+
+set(REFL_CPP_DIR ${CMAKE_SOURCE_DIR}/../refl-cpp CACHE PATH "Directory of refl-cpp")
+
+find_path(REFL_CPP_INCLUDE refl.hpp ${REFL_CPP_DIR})
+
+if("${REFL_CPP_INCLUDE}" STREQUAL "REFL_CPP_INCLUDE-NOTFOUND")
+	message(SEND_ERROR "Could not find refl.hpp, please specify REFL_CPP_DIR")
+else()
+	message("Found refl.hpp in:'${REFL_CPP_INCLUDE}' ")
+endif()
+
+add_library(reflCpp::reflCpp INTERFACE IMPORTED)
+target_include_directories(reflCpp::reflCpp INTERFACE ${REFL_CPP_INCLUDE})

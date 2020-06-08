04eddec9437dca982c16c4205f763e13e19c7842
Christian von Arnim <christian.von-arnim@isw.uni-stuttgart.de>
2020-06-08 09:01:28 +0200

    Extend defined types

--- MT.hpp
+++ MT.hpp
@@ -69,7 +69,7 @@ REFL_TYPE(IMachineVendorNameplate_t,
           Bases<IVendorNameplate_t>(),
           open62541Cpp::attribute::UaObjectType{.NodeId = open62541Cpp::constexp::NodeId(constants::NsMachineryUri, UA_MACHINERY_ID_IMACHINEVENDORNAMEPLATETYPE)})
 REFL_FIELD(YearOfConstruction, open62541Cpp::attribute::PlaceholderOptional(),
-  open62541Cpp::attribute::MemberInTypeNodeId {.NodeId = open62541Cpp::constexp::NodeId(constants::NsMachineryUri, UA_MACHINERY_ID_IMACHINEVENDORNAMEPLATETYPE_YEAROFCONSTRUCTION)})
+           open62541Cpp::attribute::MemberInTypeNodeId{.NodeId = open62541Cpp::constexp::NodeId(constants::NsMachineryUri, UA_MACHINERY_ID_IMACHINEVENDORNAMEPLATETYPE_YEAROFCONSTRUCTION)})
 REFL_FIELD(Manufacturer);
 REFL_FIELD(ProductInstanceUri);
 REFL_FIELD(SerialNumber);
@@ -164,6 +164,124 @@ REFL_TYPE(NotificationEvent_t,
 REFL_FIELD(Identifier)
 REFL_END
 
+struct StateVariable_t
+{
+  // id omitted and defined in subsequent types
+  open62541Cpp::LocalizedText_t Value;
+  UA_QualifiedName Name;
+  UA_UInt32 Number;
+  open62541Cpp::LocalizedText_t EffectiveDisplayName;
+};
+
+REFL_TYPE(StateVariable_t,
+          open62541Cpp::attribute::UaVariableType{.NodeId = open62541Cpp::constexp::NodeId(constants::Ns0Uri, UA_NS0ID_STATEVARIABLETYPE)})
+REFL_FIELD(Value, open62541Cpp::attribute::UaVariableTypeValue())
+REFL_FIELD(Name, open62541Cpp::attribute::PlaceholderOptional())
+REFL_FIELD(Number, open62541Cpp::attribute::PlaceholderOptional())
+REFL_FIELD(EffectiveDisplayName, open62541Cpp::attribute::PlaceholderOptional())
+REFL_END
+
+struct TwoStateVariable_t : public StateVariable_t
+{
+  bool Id;
+  open62541Cpp::DateTime_t EffectiveTransitionTime;
+  open62541Cpp::DateTime_t TransitionTime;
+  open62541Cpp::LocalizedText_t FalseState;
+  open62541Cpp::LocalizedText_t TrueState;
+};
+
+REFL_TYPE(TwoStateVariable_t,
+          Bases<StateVariable_t>(),
+          open62541Cpp::attribute::UaVariableType{.NodeId = open62541Cpp::constexp::NodeId(constants::Ns0Uri, UA_NS0ID_TWOSTATEVARIABLETYPE)})
+REFL_FIELD(Id)
+REFL_FIELD(EffectiveTransitionTime, open62541Cpp::attribute::PlaceholderOptional())
+REFL_FIELD(TransitionTime, open62541Cpp::attribute::PlaceholderOptional())
+REFL_FIELD(FalseState, open62541Cpp::attribute::PlaceholderOptional())
+REFL_FIELD(TrueState, open62541Cpp::attribute::PlaceholderOptional())
+REFL_END
+
+template <typename T>
+struct ConditionVariable_t
+{
+  T Value;
+  open62541Cpp::DateTime_t SourceTimestamp;
+};
+
+REFL_TEMPLATE((typename T), (ConditionVariable_t<T>), open62541Cpp::attribute::UaVariableType{.NodeId = open62541Cpp::constexp::NodeId(constants::Ns0Uri, UA_NS0ID_CONDITIONVARIABLETYPE)})
+REFL_FIELD(Value, open62541Cpp::attribute::UaVariableTypeValue())
+REFL_FIELD(SourceTimestamp)
+REFL_END
+
+struct Condition_t : public BaseEventType_t
+{
+  UA_NodeId BranchId;
+  std::string ClientUserId;
+  ConditionVariable_t<open62541Cpp::LocalizedText_t> Comment;
+  UA_NodeId ConditionClassId;
+  open62541Cpp::LocalizedText_t ConditionClassName;
+  std::string ConditionName;
+  UA_NodeId ConditionSubClassId;
+  open62541Cpp::LocalizedText_t ConditionSubClassName;
+  TwoStateVariable_t EnabledState;
+  ConditionVariable_t<open62541Cpp::LocalizedText_t> LastSeverity;
+  ConditionVariable_t<UA_StatusCode> Quality;
+  bool Retain;
+};
+
+REFL_TYPE(Condition_t,
+          Bases<BaseEventType_t>(),
+          open62541Cpp::attribute::UaObjectType{.NodeId = open62541Cpp::constexp::NodeId(constants::Ns0Uri, UA_NS0ID_CONDITIONTYPE)})
+REFL_FIELD(BranchId)
+REFL_FIELD(ClientUserId)
+REFL_FIELD(Comment)
+REFL_FIELD(ConditionClassId)
+REFL_FIELD(ConditionClassName)
+REFL_FIELD(ConditionName)
+REFL_FIELD(ConditionSubClassId, open62541Cpp::attribute::PlaceholderOptional())
+REFL_FIELD(ConditionSubClassName, open62541Cpp::attribute::PlaceholderOptional())
+REFL_FIELD(EnabledState)
+REFL_FIELD(LastSeverity)
+REFL_FIELD(Quality)
+REFL_END
+
+class AcknowledgeableCondition_t : public Condition_t
+{
+public:
+  BindableMember<TwoStateVariable_t> EnabledState;
+  BindableMember<TwoStateVariable_t> AckedState;
+  BindableMember<TwoStateVariable_t> ConfirmedState;
+};
+
+REFL_TYPE(AcknowledgeableCondition_t,
+          Bases<Condition_t>(),
+          open62541Cpp::attribute::UaObjectType{.NodeId = open62541Cpp::constexp::NodeId(constants::Ns0Uri, UA_NS0ID_ACKNOWLEDGEABLECONDITIONTYPE)})
+REFL_FIELD(EnabledState, open62541Cpp::attribute::MemberInTypeNodeId{.NodeId = open62541Cpp::constexp::NodeId(constants::Ns0Uri, UA_NS0ID_ACKNOWLEDGEABLECONDITIONTYPE_ENABLEDSTATE)})
+REFL_FIELD(AckedState, open62541Cpp::attribute::MemberInTypeNodeId{.NodeId = open62541Cpp::constexp::NodeId(constants::Ns0Uri, UA_NS0ID_ACKNOWLEDGEABLECONDITIONTYPE_ACKEDSTATE)})
+REFL_FIELD(ConfirmedState, open62541Cpp::attribute::MemberInTypeNodeId{.NodeId = open62541Cpp::constexp::NodeId(constants::Ns0Uri, UA_NS0ID_ACKNOWLEDGEABLECONDITIONTYPE_CONFIRMEDSTATE)}, open62541Cpp::attribute::PlaceholderOptional())
+REFL_END
+
+struct AlarmCondition_t : public AcknowledgeableCondition_t
+{
+  ///\todo declare fields
+};
+
+REFL_TYPE(AlarmCondition_t,
+          Bases<AcknowledgeableCondition_t>(),
+          open62541Cpp::attribute::UaObjectType{.NodeId = open62541Cpp::constexp::NodeId(constants::Ns0Uri, UA_NS0ID_ALARMCONDITIONTYPE)})
+
+REFL_END
+
+struct Alert_t : public AlarmCondition_t
+{
+  std::string ErrorCode;
+};
+
+REFL_TYPE(Alert_t,
+          Bases<AlarmCondition_t>(),
+          open62541Cpp::attribute::UaObjectType{.NodeId = open62541Cpp::constexp::NodeId(constants::NsMachineToolUri, UA_MACHINETOOLID_ALERTTYPE)})
+REFL_FIELD(ErrorCode)
+REFL_END
+
 struct ProductionJob_t
 {
   BindableMemberValue<std::string> Identifier;
@@ -206,17 +324,18 @@ struct Production_t
 
 REFL_TYPE(Production_t, open62541Cpp::attribute::UaObjectType{.NodeId = open62541Cpp::constexp::NodeId(constants::NsMachineToolUri, UA_MACHINETOOLID_PRODUCTIONTYPE)})
 REFL_FIELD(ProductionPlan, open62541Cpp::attribute::PlaceholderOptional(),
-open62541Cpp::attribute::MemberInTypeNodeId {.NodeId = open62541Cpp::constexp::NodeId(constants::NsMachineToolUri, UA_MACHINETOOLID_PRODUCTIONTYPE_PRODUCTIONPLAN)})
+           open62541Cpp::attribute::MemberInTypeNodeId{.NodeId = open62541Cpp::constexp::NodeId(constants::NsMachineToolUri, UA_MACHINETOOLID_PRODUCTIONTYPE_PRODUCTIONPLAN)})
 REFL_END
 
-namespace MT {
-  struct MachineTool_t
-  {
-    BindableMember<MachineToolIdentification_t> Identification;
-    BindableMember<Monitoring_t> Monitoring;
-    BindableMember<Production_t> Production;
-  };
-}
+namespace MT
+{
+struct MachineTool_t
+{
+  BindableMember<MachineToolIdentification_t> Identification;
+  BindableMember<Monitoring_t> Monitoring;
+  BindableMember<Production_t> Production;
+};
+} // namespace MT
 
 REFL_TYPE(MT::MachineTool_t, open62541Cpp::attribute::UaObjectType{.NodeId = open62541Cpp::constexp::NodeId(constants::NsMachineToolUri, UA_MACHINETOOLID_MACHINETOOLTYPE)})
 REFL_FIELD(Identification, open62541Cpp::attribute::UaBrowseName{.NsURI = constants::NsDIUri})

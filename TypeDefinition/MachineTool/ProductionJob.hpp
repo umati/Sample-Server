e9f1e26c30a8a7f5ccd19f9f03371ffbb351439c
Tonja Heinemann <tonja.heinemann@isw.uni-stuttgart.de>
2020-08-20 14:25:25 +0200

    added IsValid to RunsPlanned

--- TypeDefinition/MachineTool/ProductionJob.hpp
+++ TypeDefinition/MachineTool/ProductionJob.hpp
@@ -17,11 +17,17 @@ namespace machineTool
     BindableMemberPlaceholder<BindableMember, ProductionPartSet_t> PartSet;
   };
 
+  struct ProductionJob_RunsPlanned_t
+  {
+    BindableMemberValue<bool> IsValid;
+    BindableMemberValue<std::uint32_t> Value;
+  };
+
   struct ProductionJob_t : public ns0::IOrderedObject_t<std::uint16_t>
   {
     BindableMemberValue<std::string> Identifier;
     BindableMemberValue<std::uint32_t> RunsCompleted;
-    BindableMemberValue<std::uint32_t> RunsPlanned;
+    BindableMember<ProductionJob_RunsPlanned_t> RunsPlanned;
     BindableMember<ProductionStateMachine_t> State;
     BindableMember<ProductionJob_ProductionPrograms_t> ProductionPrograms;
     BindableMember<ProductionJob_PartSets_t> PartSets;
@@ -42,6 +48,19 @@ REFL_FIELD(PartSet,
             UmatiServerLib::attribute::MemberInTypeNodeId{
                .NodeId = UmatiServerLib::constexp::NodeId(constants::NsMachineToolUri, UA_MACHINETOOLID_PRODUCTIONJOBTYPE_PARTSETS_PARTSET)},
            UmatiServerLib::attribute::PlaceholderOptional())
+REFL_END
+
+REFL_TYPE(machineTool::ProductionJob_RunsPlanned_t,
+          UmatiServerLib::attribute::UaVariableType{
+              .NodeId = UmatiServerLib::constexp::NodeId(constants::Ns0Uri, UA_NS0ID_BASEVARIABLETYPE)})
+REFL_FIELD(Value,
+           UmatiServerLib::attribute::MemberInTypeNodeId{
+               .NodeId = UmatiServerLib::constexp::NodeId(constants::NsMachineToolUri, UA_MACHINETOOLID_PRODUCTIONJOBTYPE_RUNSPLANNED)},
+           UmatiServerLib::attribute::UaVariableTypeValue())
+REFL_FIELD(IsValid,
+           UmatiServerLib::attribute::UaBrowseName{.NsURI = constants::NsMachineToolUri},
+           UmatiServerLib::attribute::MemberInTypeNodeId{
+               .NodeId = UmatiServerLib::constexp::NodeId(constants::NsMachineToolUri, UA_MACHINETOOLID_PRODUCTIONJOBTYPE_RUNSPLANNED_ISVALID)})
 REFL_END
 
 REFL_TYPE(machineTool::ProductionJob_t,

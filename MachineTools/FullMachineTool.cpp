e6cfc52b3abf6bd66a3ce0942047d90dac86831b
Götz Görisch <47734341+GoetzGoerisch@users.noreply.github.com>
2021-02-23 10:50:56 +0100

    feat(MT): update MT Location for upcoming METAV

--- MachineTools/FullMachineTool.cpp
+++ MachineTools/FullMachineTool.cpp
@@ -119,7 +119,7 @@ void FullMachineTool::InstantiateIdentification()
   mt.Identification->ComponentName = {"", MachineName};
   mt.Identification->ProductCode = "2020-47110815";
   mt.Identification->SoftwareRevision= "1.00.1";
-  mt.Identification->Location = "AMB 1 A1/VIRTUAL 0 0/N 48.781340 E 9.165731";
+  mt.Identification->Location = "METAV 1 A1/VIRTUAL 0 0/N 48.781340 E 9.165731";
   mt.Identification->Model = {"", MachineName};
 
   auto &swOS = mt.Identification->SoftwareIdentification->SoftwareItem.Add(m_pServer, n, {m_nsIndex, "OS"});

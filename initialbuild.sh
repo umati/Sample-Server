de525710b0129da71b96015c73729546328d9446
Götz Görisch <47734341+GoetzGoerisch@users.noreply.github.com>
2021-05-31 06:21:10 +0000

    fix(build): Set build type for faster pre-builds

--- initialbuild.sh
+++ initialbuild.sh
@@ -4,7 +4,7 @@ BASEDIR=$(pwd)
 
 mkdir build
 cd build || exit
-cmake -DCMAKE_INSTALL_PREFIX:PATH="$BASEDIR/install" ../.github/ 
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX:PATH="$BASEDIR/install" ../.github/ 
 cmake --build .
 cd SampleServer-build || exit
 ctest

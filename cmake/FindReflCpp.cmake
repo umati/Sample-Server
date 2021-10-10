8b49d204ec798f99fc9d3cf6be1bc5a7352a5bb0
Dennis Hezel <dh@3yourmind.com>
2021-10-10 13:27:07 +0200

    chore: Add and apply cmake-format

--- cmake/FindReflCpp.cmake
+++ cmake/FindReflCpp.cmake
@@ -1,12 +1,14 @@
-
-set(REFL_CPP_DIR ${CMAKE_SOURCE_DIR}/deps/refl-cpp/include/ CACHE PATH "Directory of refl-cpp")
+set(REFL_CPP_DIR
+    ${CMAKE_SOURCE_DIR}/deps/refl-cpp/include/
+    CACHE PATH "Directory of refl-cpp"
+)
 
 find_path(REFL_CPP_INCLUDE refl.hpp ${REFL_CPP_DIR})
 
 if("${REFL_CPP_INCLUDE}" STREQUAL "REFL_CPP_INCLUDE-NOTFOUND")
-	message(SEND_ERROR "Could not find refl.hpp, please specify REFL_CPP_DIR")
+    message(SEND_ERROR "Could not find refl.hpp, please specify REFL_CPP_DIR")
 else()
-	message("Found refl.hpp in:'${REFL_CPP_INCLUDE}' ")
+    message("Found refl.hpp in:'${REFL_CPP_INCLUDE}' ")
 endif()
 
 add_library(reflCpp::reflCpp INTERFACE IMPORTED)

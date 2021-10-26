26c77078d598d5933458b04cc4babd4b0979e93c
Götz Görisch <47734341+GoetzGoerisch@users.noreply.github.com>
2021-10-26 15:17:46 +0000

    feat(Server): Updates initialbuild to use LibreSSL

--- initialbuild.sh
+++ initialbuild.sh
@@ -23,6 +23,9 @@
 
 BASEDIR=$(pwd)
 
+cd deps/libressl/portable || exit
+./autogen.sh
+cd $BASEDIR || exit
 mkdir build
 cd build || exit
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX:PATH="$BASEDIR/install" ../.github/ 

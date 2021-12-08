4596f8003b1c94f1c4facdc106fab2e3e503abab
Götz Görisch <47734341+GoetzGoerisch@users.noreply.github.com>
2021-12-08 15:48:34 +0000

    fix(lib): Updates headers

--- MachineTools/MRMachineTool.cpp
+++ MachineTools/MRMachineTool.cpp
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
  * Copyright 2020-2021 (c) Christian von Arnim, ISW University of Stuttgart (for umati and VDW e.V.)
+ * Copyright 2020 (c) Tonja Heinemann, ISW University of Stuttgart (for umati and VDW e.V.)
  */
 
 #include "MRMachineTool.hpp"

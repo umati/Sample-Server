546a02b5d5b30c2fcf814f516b821e924042e441
Sebastian Friedl <sebastian.friedl@isw.uni-stuttgart.de>
2022-02-08 21:19:50 +0100

    feat(GMS): Adds Corrections

---
+++ TypeDefinition/GMS/CorrectionType.hpp
@@ -0,0 +1,65 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 2022 (c) Sebastian Friedl, ISW University of Stuttgart (for VDMA e.V.)
+ */
+
+#pragma once
+#include "../TypeDefinition.hpp"
+#include "../ns0/AnalogUnitType.hpp"
+#include "Constants.hpp"
+
+namespace GMS {
+    struct CorrectionType_t  {
+        BindableMemberValue<std::string> CharacteristicIdentfier;
+        BindableMember<ns0::AnalogUnitType_t<double>>CorrectionValueAbsolute;
+        BindableMember<ns0::AnalogUnitType_t<double>>CorrectionValueRelative;
+        BindableMemberValue<std::string> Description;
+        BindableMemberValue<std::string> Identifier;
+        BindableMember<ns0::AnalogUnitType_t<double>> LowerControlLimit;
+        BindableMember<ns0::AnalogUnitType_t<double>> UpperControlLimit;
+
+    };
+}  // namespace GMS
+
+REFL_TYPE(
+        GMS::CorrectionType_t,
+        UmatiServerLib::attribute::UaObjectType(UmatiServerLib::constexp::NodeId(constants::NsGMSUri, UA_GMSID_CORRECTIONTYPE))
+        )
+        REFL_FIELD(
+                CharacteristicIdentfier,
+                UmatiServerLib::attribute::MemberInTypeNodeId(UmatiServerLib::constexp::NodeId(constants::NsGMSUri, UA_GMSID_CORRECTIONTYPE_CHARACTERISTICIDENTFIER))
+                )
+        REFL_FIELD(
+                CorrectionValueAbsolute,
+                UmatiServerLib::attribute::MemberInTypeNodeId(UmatiServerLib::constexp::NodeId(constants::NsGMSUri, UA_GMSID_CORRECTIONTYPE_CORRECTIONVALUEABSOLUTE)),
+                UmatiServerLib::attribute::PlaceholderOptional()
+        )
+        REFL_FIELD(
+                CorrectionValueRelative,
+                UmatiServerLib::attribute::MemberInTypeNodeId(UmatiServerLib::constexp::NodeId(constants::NsGMSUri, UA_GMSID_CORRECTIONTYPE_CORRECTIONVALUERELATIVE)),
+                UmatiServerLib::attribute::PlaceholderOptional()
+        )
+        REFL_FIELD(
+                Description,
+                UmatiServerLib::attribute::MemberInTypeNodeId(UmatiServerLib::constexp::NodeId(constants::NsGMSUri, UA_GMSID_CORRECTIONTYPE_DESCRIPTION)),
+                UmatiServerLib::attribute::PlaceholderOptional()
+        )
+        REFL_FIELD(
+                Identifier,
+                UmatiServerLib::attribute::MemberInTypeNodeId(UmatiServerLib::constexp::NodeId(constants::NsGMSUri, UA_GMSID_CORRECTIONTYPE_IDENTIFIER))
+        )
+        REFL_FIELD(
+                LowerControlLimit,
+                UmatiServerLib::attribute::MemberInTypeNodeId(UmatiServerLib::constexp::NodeId(constants::NsGMSUri, UA_GMSID_CORRECTIONTYPE_LOWERCONTROLLIMIT)),
+                UmatiServerLib::attribute::PlaceholderOptional()
+        )
+        REFL_FIELD(
+                UpperControlLimit,
+                UmatiServerLib::attribute::MemberInTypeNodeId(UmatiServerLib::constexp::NodeId(constants::NsGMSUri, UA_GMSID_CORRECTIONTYPE_UPPERCONTROLLIMIT)),
+                UmatiServerLib::attribute::PlaceholderOptional()
+        )
+REFL_END
+
+
--- TypeDefinition/GMS/GMSResultManagementType.hpp
+++ TypeDefinition/GMS/GMSResultManagementType.hpp
@@ -9,15 +9,43 @@
 #include "../TypeDefinition.hpp"
 #include "Constants.hpp"
 #include "../MachineryResult/ResultManagementType.hpp"
+#include "CorrectionType.hpp"
 
 namespace GMS {
+
+    struct GMSResultManagementType_CorrectionsFolder_t {
+        BindableMemberPlaceholder<BindableMember, CorrectionType_t> Corrections;
+    };
+
+
     struct GMSResultManagementType_t : public machinery_result::ResultManagementType_t {
+        BindableMember<GMSResultManagementType_CorrectionsFolder_t> CorrectionsFolder;
 
     };
 }  // namespace GMS
 
+
+REFL_TYPE(
+        GMS::GMSResultManagementType_CorrectionsFolder_t,
+        UmatiServerLib::attribute::UaObjectType(  // TODO handle correctly when this is pointing to an object instead of an object type
+                UmatiServerLib::constexp::NodeId(constants::Ns0Uri, UA_NS0ID_FOLDERTYPE))
+)
+        REFL_FIELD(
+                Corrections,
+                UmatiServerLib::attribute::PlaceholderOptional(),
+                UmatiServerLib::attribute::MemberInTypeNodeId(
+                        UmatiServerLib::constexp::NodeId(constants::NsGMSUri, UA_GMSID_GMSRESULTMANAGEMENTTYPE_CORRECTIONSFOLDER_CORRECTIONS))
+        )
+REFL_END
+
 REFL_TYPE(
         GMS::GMSResultManagementType_t,
         Bases<machinery_result::ResultManagementType_t>(),
-        UmatiServerLib::attribute::UaObjectType(UmatiServerLib::constexp::NodeId(constants::NsGMSUri, UA_GMSID_GMSRESULTMANAGEMENTTYPE)))
+        UmatiServerLib::attribute::UaObjectType(UmatiServerLib::constexp::NodeId(constants::NsGMSUri, UA_GMSID_GMSRESULTMANAGEMENTTYPE))
+        )
+        REFL_FIELD(
+                CorrectionsFolder,
+                UmatiServerLib::attribute::MemberInTypeNodeId(UmatiServerLib::constexp::NodeId(constants::NsGMSUri, UA_GMSID_GMSRESULTMANAGEMENTTYPE_CORRECTIONSFOLDER)),
+                UmatiServerLib::attribute::PlaceholderOptional()
+        )
 REFL_END

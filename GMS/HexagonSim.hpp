9c4c07900a794f6c6c3277e0b5457a2b12e15b65
Karsten Röthig <karstenroethig@users.noreply.github.com>
2022-04-25 14:36:31 +0200

    feat(GMS): Added sample machine for Hexagon

---
+++ GMS/HexagonSim.hpp
@@ -0,0 +1,33 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 2022 (c) Sebastian Friedl, ISW University of Stuttgart (for VDMA e.V.)
+ */
+
+#pragma once
+#include "../MachineTools/InstantiatedMachineTool.hpp"
+#include "../TypeDefinition/GMS/GMSType.hpp"
+
+class HexagonSim : public InstantiatedMachineTool {
+ public:
+  HexagonSim(UA_Server *pServer);
+
+  void Simulate() override;
+  virtual ~HexagonSim() = default;
+
+ protected:
+  void CreateObject() override;
+
+  GMS::GMS_t mt;
+  UA_String m_resulturi[1];
+  void InstantiateIdentification();
+  void InstantiateMonitoring();
+  void InstantiateProduction();
+  void InstantiateTools();
+  int m_simStep = 0;
+
+  void initCorrection(GMS::CorrectionType_t &corr, std::string Identifier, std::string CharacteristicIdentfier, double value);
+
+  void InstantiateResultManagement();
+};

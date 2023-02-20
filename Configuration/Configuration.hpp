1dbc25eb4e7b7b6abeb8ad5d579ee5f9e5ed6801
Moritz Walker <moritz.walker@isw.uni-stuttgart.de>
2023-02-20 14:48:39 +0100

    feat: working ads connection

--- Configuration/Configuration.hpp
+++ Configuration/Configuration.hpp
@@ -10,6 +10,8 @@
 #include <optional>
 #include <string>
 #include <vector>
+#include <array>
+#include <sstream>
 
 namespace Configuration {
 struct Encryption_t {
@@ -39,12 +41,41 @@ struct MQTTPubSubConfiguration_t {
   std::string PublisherId;
 };
 
+struct ADSConfiguration_t {
+  std::array<uint8_t, 6> LocalNetId;
+  std::array<uint8_t, 6> RemoteNetId;
+  uint16_t RemotePort; 
+  
+  std::string getLocalNetId() {
+    std::stringstream ss;
+    ss << unsigned(LocalNetId[0]);
+
+    for(auto mIter = std::next(LocalNetId.begin()); mIter != LocalNetId.end(); ++mIter) {
+      ss << '.' << unsigned(*mIter);
+    }
+
+    return ss.str();
+  }
+
+  std::string getRemoteNetId() {
+    std::stringstream ss;
+    ss << unsigned(RemoteNetId[0]);
+
+    for(auto mIter = std::next(RemoteNetId.begin()); mIter != (RemoteNetId.end() - 2); ++mIter) {
+      ss << '.' << unsigned(*mIter);
+    }
+
+    return ss.str();
+  }
+};
+
 struct Configuration {
   std::optional<std::string> Hostname;
   std::optional<std::uint16_t> Port;
   std::optional<std::vector<UserPassAuthentication_t>> UserPassAuthentication;
   std::optional<Encryption_t> Encryption;
   std::optional<MQTTPubSubConfiguration_t> MQTTPubSub;
+  std::optional<ADSConfiguration_t> Ads;
 };
 
 Configuration DefaultConfiguration();

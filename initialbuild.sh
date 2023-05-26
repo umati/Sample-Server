4932bd581602df0153d2cfe4fa5b23dd0afca111
Christian von Arnim <ccvca@users.noreply.github.com>
2023-05-26 11:45:13 +0200

    fix: Match order of importing namespaces with CMakeLists (#1076)

--- initialbuild.sh
+++ initialbuild.sh
@@ -23,8 +23,8 @@
 
 BASEDIR=$(pwd)
 
-mkdir build
-cd build || exit
+mkdir build-auto
+cd build-auto || exit
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX:PATH="$BASEDIR/install" ../.github/ 
 cmake --build .
 cd SampleServer-build || exit

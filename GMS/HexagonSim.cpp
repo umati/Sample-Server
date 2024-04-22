3cfe0c7478e3aa09a4ea71d116331d814fa99910
Goetz Goerisch <g.goerisch@vdw.de>
2024-04-22 06:47:13 +0000

    feat: HMI 2024

--- GMS/HexagonSim.cpp
+++ GMS/HexagonSim.cpp
@@ -94,7 +94,7 @@ void HexagonSim::InstantiateIdentification() {
   mt.Identification->YearOfConstruction = 2021;
   mt.Identification->SoftwareRevision = "Quindos7 - V 7.22.22096";
   mt.Identification->DeviceClass = "Coordinate Measuring Machine";
-  mt.Identification->Location = "EMO 6 B65/VIRTUAL 0 0/N 50.5514144 E 8.4764728";
+  mt.Identification->Location = "HMI 8 D16/EMO 6 B65/VIRTUAL 0 0/N 50.5514144 E 8.4764728";
   mt.Identification->Model = {"", MachineName};
   mt.Identification->SerialNumber = "0104";
 }

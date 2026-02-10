1fadb43d85c38c0ef32d30b7cb22cdc11b31d680
Goetz Goerisch <g.goerisch@vdw.de>
2026-02-10 14:25:11 +0100

    feat: set location for TMTS2026
    Signed-off-by: Goetz Goerisch <g.goerisch@vdw.de>

--- GMS/OGPSmartScopeCNC500.cpp
+++ GMS/OGPSmartScopeCNC500.cpp
@@ -134,7 +134,7 @@ void OGPSmartScopeCNC500::InstantiateIdentification() {
   mt.Identification->YearOfConstruction = 2022;
   mt.Identification->SoftwareRevision = "V1.0.0";
   mt.Identification->DeviceClass = "GMS";
-  mt.Identification->Location = "EMO 6 A18/VIRTUAL 1 1/N 48.694241 E 9.183833";
+  mt.Identification->Location = "TMTS 4 G0912/VIRTUAL 1 1/N 48.694241 E 9.183833";
   mt.Identification->Model = {"", MachineName};
 
   {

9e80e6dc7d597ccf6fa9d884bde8872c31d79752
Goetz Goerisch <g.goerisch@vdw.de>
2026-08-07 14:59:04 +0200

    fix(build): fix window-2025 build, bump cmake min version (#1749)
    Signed-off-by: Goetz Goerisch <g.goerisch@vdw.de>
    Reviewed-on: https://codeberg.org/umati/Sample-Server/pulls/1749

--- cmake/FindReflCpp.cmake
+++ cmake/FindReflCpp.cmake
@@ -1,16 +1,16 @@
 # MIT License
-# 
+#
 # Copyright 2020-2021 (c) Christian von Arnim, ISW University of Stuttgart (for umati and VDW e.V.)
 # Copyright 2021 (c) Götz Görisch, VDW - Verein Deutscher Werkzeugmaschinenfabriken e.V.
 # Copyright 2021 (c) Dennis Hezel, 3Yourmind GmbH
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 #
@@ -21,7 +21,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
 
 set(REFL_CPP_DIR
     ${CMAKE_SOURCE_DIR}/deps/refl-cpp/include/
